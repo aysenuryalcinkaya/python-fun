053def38f0fb1419be14a356ea3cca78ac9fda33
aysenuryalcinkaya <90062794+aysenuryalcinkaya@users.noreply.github.com>
2023-06-21 15:56:58 +0300

    Add files via upload

---
+++ Fore.py
@@ -0,0 +1,11 @@
+from colorama import Fore
+
+print(Fore.RED + 'Hello World!')
+print(Fore.GREEN + 'Hello World!')
+print(Fore.YELLOW + 'Hello World!')
+print(Fore.BLUE + 'Hello World!')
+print(Fore.MAGENTA + 'Hello World!')
+print(Fore.CYAN + 'Hello World!')
+print(Fore.WHITE + 'Hello World!')
+
+
